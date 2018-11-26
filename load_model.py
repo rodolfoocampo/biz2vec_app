cdae4c79a401d02aa2b2df63b114acee456cf089
Rodolfo Ocampo <jrocampo@jrocampo-macbookpro.roam.corp.google.com>
2018-11-25 18:04:54 -0600

    first deploy

--- load_model.py
+++ load_model.py
@@ -1,10 +1,14 @@
 from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 from flask import Flask
+from flask import request, render_template, flash
 app = Flask(__name__)
+
 @app.route('/<biz_code>')
 def hello_world(biz_code):
 	model = Word2Vec.load("word2vec.model")
 	prediction = model.predict_output_word([str(biz_code)])
 	print(prediction)
 	return (str(prediction))
+
+
