46bda33344f77b01c65a75b7d33dc3d2b4aeba01
Rodolfo Ocampo <jrocampo@jrocampo-macbookpro.roam.corp.google.com>
2018-11-25 19:26:14 -0600

    Fix route error

--- load_model.py
+++ load_model.py
@@ -2,6 +2,9 @@ from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 from flask import Flask
 from flask import request, render_template, flash
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/<biz_code>')
@@ -10,5 +13,14 @@ def hello_world(biz_code):
 	prediction = model.predict_output_word([str(biz_code)])
 	print(prediction)
 	return (str(prediction))
+
+
+
+
+
+@app.route('/')
+
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
