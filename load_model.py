0a228633558b9d80e7c39e802c021f1f4d74e929
Rodolfo Ocampo <jrocampo@jrocampo-macbookpro.roam.corp.google.com>
2018-12-08 17:16:30 -0600

    Think post call works now, only testing if coordinate parsing for now

--- load_model.py
+++ load_model.py
@@ -8,6 +8,35 @@ import io
 
 app = Flask(__name__)
 
+
+## Parse coordinate 
+
+
+
+@app.route('/parse-coordinate', methods=['POST', 'GET'])
+def show_user_profile():
+
+    if request.method == 'POST':
+
+    # checar si hay que parsear el request a texto
+
+      coordinates = request.form['location']
+      #coord2 = coordinates.replace("(","")
+      #coord3 = coord2.replace(")","")
+      #coord4 = coord3.replace(".","")
+      #coord5 = coord4.replace(",","")
+      #coord6 = coord5.replace(" ","")
+      #url = "https://maps.googleapis.com/maps/api/staticmap?center=" + coord3 + "&zoom=18&size=600x300&maptype=satellite&format=jpg&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAEzAp00zKY3XiNRTBaNKaUz9k-_9caWLw"
+
+      #image_location = "/Users/rodolfoocampo/Documents/Datalab/Proyectos/tensorflow-for-poets-2/real_images/" + coord6 + ".jpg"
+      #urllib.urlretrieve(url, image_location)
+      print(coordinates)
+      output = coordinates
+      resp = flask.Response(output)
+      resp.headers['Access-Control-Allow-Origin'] = '*'
+      return resp
+    return
+
 @app.route('/model?coordinate=<biz_code>')
 def hello_world(biz_code):
 	model = Word2Vec.load("word2vec.model")
